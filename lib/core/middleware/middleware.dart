bff122868efd294af861b7f2a4a599c1156ce387
shosho <shahdmkz.o@gmail.com>
2024-05-21 17:05:57 +0300

    myFramework

---
+++ lib/core/middleware/middleware.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import '../constants/approutes.dart';
+import '../services/services.dart';
+
+class MyMiddleWare extends GetMiddleware {
+  MyServices myServices = Get.find();
+
+  @override
+  RouteSettings? redirect(String? route) {
+    if (myServices.sharedPreferences.getString("step") == "2") {
+      return const RouteSettings(name: AppRoutes.home);
+    }
+    if (myServices.sharedPreferences.getString("step") == "1") {
+      return const RouteSettings(name: AppRoutes.login);
+    }
+    return null;
+  }
+}
